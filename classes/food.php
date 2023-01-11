23782c86aeb74886e28651872599aa3cc9f9d7d0
SimoVisc <simonaviscardivs@gmail.com>
2023-01-11 18:01:38 +0100

    Aggiunge la classe food

--- classes/food.php
+++ classes/food.php
@@ -1,18 +1,31 @@
 <?php
- class Food{
-  public $name;
-  public $categories;
-  public $price;
-  public $ingredients;
-  
+require_once __DIR__ . "/product.php";
+class Food extends Product{
+  private $brand;
+  private $ingredient;
 
-  public function __construct(string $_name , array $_category , int $_price){
-    $this -> name = $_name;
-    $this -> categories = $_category;
-    $this -> price = $_price;
+  public function __construct(string $name, string $image, float $price, Category $category ){
+    parent::__construct($name,$image,$price,$category);
   }
-  public function getInfo(){
-    return "ingredienti: {$this->ingredients} ";
+
+  public function setBrand($brand)
+  {
+    $this -> brand = $brand;
+  }
+
+  public function getBrand()
+  {
+    return $this -> brand;
+  }
+
+  public function setIngredient($ingredient)
+  {
+    $this -> ingredient = $ingredient;
+  }
+
+  public function getIngredient()
+  {
+    return $this -> ingredient;
   }
 }
 ?>
