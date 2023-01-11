3495e520667e4944edd93bfaa0329101a8a31932
SimoVisc <simonaviscardivs@gmail.com>
2023-01-11 17:56:50 +0100

    Aggiunge la classe index

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ ."/classes/product.php";
+require_once __DIR__ ."/classes/food.php";
+require_once __DIR__ ."/classes/toy.php";
+require_once __DIR__ ."/classes/kennel.php";
+require_once __DIR__ ."/classes/category.php";
+
